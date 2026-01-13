051dc05b45267e2bf6841153944dd81687de35c9
Lucas Serejo <lucasj.serejo11@gmail.com>
2026-01-13 14:20:47 -0300

    feat: add user card component and user data

--- user-list-page/src/app/app.component.ts
+++ user-list-page/src/app/app.component.ts
@@ -1,12 +1,47 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
+import { User } from './user.interface';
+import { UserCardComponent } from '../components/user-card/user-card.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [FormsModule, UserCardComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
-  title = 'user-list-page';
+export class AppComponent implements OnInit {
+  searchTerm: string = '';
+
+  users: User[] = [];
+  filteredUsers: User[] = [];
+
+  constructor(private http: HttpClient) {}
+
+  loadUsers() {
+    this.http.get<User[]>('assets/users.json').subscribe(data => {
+      this.users = data;
+      this.filteredUsers = data;
+    });
+  }
+
+  ngOnInit() {
+    this.loadUsers();
+  }
+
+  filterUsers() {
+    if (!this.searchTerm) {
+      this.filteredUsers = [...this.users];
+      return;
+    }
+    const term = this.searchTerm.toLowerCase();
+    this.filteredUsers = this.users.filter(user => 
+      user.name.toLowerCase().includes(term) ||
+      user.email.toLowerCase().includes(term) ||
+      user.role.toLowerCase().includes(term)
+    );
+  }
+
+
+
 }
